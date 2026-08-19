4f08ef79c7296be6212348c643542b53a34f64b0
Manilko, Yevhenii <Yevhenii.Manilko@capgemini.com>
2026-08-19 21:55:07 +0300

    Initial Commit

---
+++ HT/HTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTApp.swift
+//  HT
+//
+//  Created by Manilko, Yevhenii on 19.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct HTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
